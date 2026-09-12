a7a385162757c096f9061913cd1d50294d0126a3
musadaniel <musadaniel@gmail.com>
2026-09-12 17:10:51 -0400

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='de-training-v1.0.0';
+const CORE=['./','./index.html','./styles.css','./manifest.webmanifest','./js/app.js','./js/data.js','./js/db.js','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+ if(event.request.method!=='GET')return;
+ event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(response.ok){const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy))}return response}).catch(()=>event.request.mode==='navigate'?caches.match('./index.html'):Response.error())));
+});
